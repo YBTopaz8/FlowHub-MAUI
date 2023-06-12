f8594a0966392e55882bf90e8f5817f8d81efbc7
Yvan Brunel <8brunel@gmail.com>
2023-06-12 00:24:04 -0400

    Done a layout for settings page, user can at least view their settings. Fixed issue #21

--- FlowHub.Main/AppShell.xaml.cs
+++ FlowHub.Main/AppShell.xaml.cs
@@ -1,5 +1,6 @@
 using FlowHub.Main.Views.Desktop;
 using FlowHub.Main.Views.Desktop.Expenditures;
+using FlowHub.Main.Views.Desktop.Settings;
 
 namespace FlowHub.Main;
 
@@ -14,5 +15,7 @@ public partial class AppShell : Shell
 
 		Routing.RegisterRoute(nameof(ManageExpendituresD), typeof(ManageExpendituresD));
 		Routing.RegisterRoute(nameof(UpSertExpenditurePageD), typeof(UpSertExpenditurePageD));
+
+		Routing.RegisterRoute(nameof(UserSettingsPageD), typeof(UserSettingsPageD));
 	}
 }
